f096295338145c1693edcc8e3de584ff96717d48
Lijiang Long <llong35@gatech.edu>
2020-03-23 12:41:29 -0400

    sth

--- utils/plot_accuracies.py
+++ utils/plot_accuracies.py
@@ -39,7 +39,7 @@ def plot_accuracies(results_folder):
     
     
 def main():
-    results_folder = '/data/home/llong35/data/transfer_test/animal_split/TI3_3/results'
+    results_folder = '/data/home/llong35/data/transfer_test/animal_split/MC16_2,MC6_5/results'
     plot_accuracies(results_folder)
 
 if __name__ == '__main__':
