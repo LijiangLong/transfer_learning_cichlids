5420fd488217465d5d4b72e9378376bbbdace86d
Lijiang Long <llong35@gatech.edu>
2020-03-26 23:52:01 -0400

    sth

--- utils/split_by_animal.py
+++ utils/split_by_animal.py
@@ -13,12 +13,13 @@ def create_random_spliting_train_test(annotation_file,
                                         data_folder,
                                         n_training=3,
                                         split_ratio = 0.8,
-                                        training_sample_size = 6000,
+                                        training_sample_size = 10000,
                                         val_sample_size = 1000,
                                         test_sample_size = 1000):
-    animals_list = ['MC16_2', 'MC6_5', 'MCxCVF1_12a_1', 'MCxCVF1_12b_1', 'TI2_4', 'TI3_3', 'CV10_3']
-    training = np.sort(np.random.choice(animals_list, n_training, replace=False))
-    result_dir = os.path.join(master_dir,','.join(training))
+#     animals_list = ['MC16_2', 'MC6_5', 'MCxCVF1_12a_1', 'MCxCVF1_12b_1', 'TI2_4', 'TI3_3', 'CV10_3']
+#     training = np.sort(np.random.choice(animals_list, n_training, replace=False))
+    training = ['MC16_2', 'MC6_5', 'MCxCVF1_12a_1', 'MCxCVF1_12b_1', 'TI2_4', 'TI3_3']
+    result_dir = os.path.join(master_dir,','.join(training)+'test')
     if os.path.isdir(result_dir):
         return
     else:
